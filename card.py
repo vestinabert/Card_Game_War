06f1ed5d0f8e06f1d92cfe4ba595d9f20adf21bd
Vestina Bertasiute <vestina.bertasiute@gmail.com>
2024-11-19 14:10:44 +0200

    issue #2

--- card.py
+++ card.py
@@ -6,3 +6,7 @@ class Card:
     def __str__(self):
         return f"{self._rank} of {self._suit}"
     
+    def compare_to(self, other_card):
+        values = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "Jack":10, "Queen":11, "King":12}
+        return values[self.rank]>values[other_card.rank]
+    
