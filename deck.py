c7a05b9c45b5aacaa72c11bd52427bb0eb7739c1
Vestina Bertasiute <vestina.bertasiute@gmail.com>
2024-11-20 20:24:44 +0200

    issue #5

--- deck.py
+++ deck.py
@@ -1,8 +1,15 @@
-from random import shuffle
+import random
+from card import Card
+
 class Deck:
-    cards = []
-    
-    @staticmethod
-    def shuffle_cards(cls):
-        shuffle(cls.cards)
+    def __init__(self):
+        self.cards = []
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(rank, suit))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
         
