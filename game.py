3a833ddf091163307a8a56a68dc0e5264199bf43
Vestina Bertasiute <vestina.bertasiute@gmail.com>
2024-11-21 09:30:51 +0200

    implementing game start function

--- game.py
+++ game.py
@@ -7,3 +7,8 @@ class Game:
         self.player1 = Player(player1_name)
         self.player2 = Player(player2_name)
         self.round = 0
+
+    def start_game(self):
+        self.deck.shuffle()
+        self.player1.hand = self.deck.deal(26)
+        self.player2.hand = self.deck.deal(26)
