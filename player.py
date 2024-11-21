67d15704fb97d5bfab611d4f98e47ae3c454a20a
Vestina Bertasiute <vestina.bertasiute@gmail.com>
2024-11-21 09:19:45 +0200

    creating player class

---
+++ player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def draw_card(self):
+        return self.hand.pop(0) if self.hand else None
+    
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def has_cards(self):
+        return len(self.hand) > 0
